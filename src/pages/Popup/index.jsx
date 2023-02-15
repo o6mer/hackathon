83acfd5e4b27dd5cbf568cc766dd8a4e8dc2549d
o6mer <o6merliraz@gmail.com>
2023-02-15 13:14:53 +0200

    reset

--- src/pages/Popup/index.jsx
+++ src/pages/Popup/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import ReactDOM from 'react-dom';
 
 import Popup from './Popup';
 import './index.css';
 
-const app = document.createElement('div');
-app.id = 'my-extension-root';
-document.body.appendChild(app);
-ReactDOM.render(<Popup />, app);
+const container = document.getElementById('app-container');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<Popup />);
