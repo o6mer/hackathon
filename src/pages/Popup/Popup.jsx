4384b4d6cc6e9d65f010c08d71d709e9449c353f
omerlevy1 <omerlevy2201@gmail.com>
2023-02-16 01:47:08 +0200

    change

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -3,101 +3,6 @@ import React from 'react';
  import img from './binicon.png'
  import './Popup.css';
 import { useState } from 'react';
- 
-
-// const Popup = () => {
-
-//   const [profiles, setProfiles] = useState([
-//     { name: "omer levy", jobTitle: "web developer", linkedin: "www.linkedin.com", checked: false },
-//     { name: "nati arbiv", jobTitle: "backend developer", linkedin: "www.linkedin.com", checked: false },
-//     { name: "omer liraz", jobTitle: "fullstack developer", linkedin: "www.linkedin.com", checked: false },
-//     { name: "asaf axelrose", jobTitle: "english developer", linkedin: "www.linkedin.com", checked: false },
-//   ]);
-
-//   const [selectAll, setSelectAll] = useState(false);
-
-//   const handleCheckboxChange = (index) => {
-//     const newProfiles = [...profiles];
-//     newProfiles[index].checked = !newProfiles[index].checked;
-//     setProfiles(newProfiles);
-//   };
-
-//   const handleSelectAllChange = (event) => {
-//     const newProfiles = profiles.map((profile) => ({
-//       ...profile,
-//       checked: event.target.checked,
-//     }));
-//     setProfiles(newProfiles);
-//     setSelectAll(event.target.checked);
-//   };
-
-//   const profileList = profiles.map((profile, index) => (
-//     <div key={index}>
-//       <input
-//         type="checkbox"
-//         checked={profile.checked}
-//         onChange={() => handleCheckboxChange(index)}
-//         className="default:ring-2 ..."
-//         id={profile.name}
-//       />
-//       <label htmlFor={profile.name}>
-//         {profile.name} <br />
-//         {profile.jobTitle} <br />
-//         {profile.linkedin}
-//         <br />
-//       </label>
-//     </div>
-//   ));
-
-//   return (
-
-
-//     <div className="App">
-
-//      <div id='img-logo-div'>
-//       <img id='img-logo-icon' src={img} alt="binoculars" />
-//       <img id='img-logo-txt' src={image}alt="Ollopa.com"/>
-//      </div>
-     
-    
-    
-//      <div id='Part2nd'>
-
-//        <div id='box-name'>
-
-//        <div>
-        
-//             <input
-//               type="checkbox"
-//               checked={selectAll}
-//               onChange={handleSelectAllChange}
-//               id="select-all"
-//             />
-//             <label id='all-select' htmlFor="select-all">Select All</label>
-//           </div>
-
-
-//         {profileList} 
-//        </div>
-
-
-// <div id='button-placement'>
-// <button id='button'>
-//   Button
-// </button>
-// </div>
-
-
-//      </div>
-     
-
-
-//     </div>
-//   );
-// };
-
-// export default Popup;
-
 
 
 
@@ -109,12 +14,21 @@ const Popup = () => {
     { name: 'asaf axelrose', jobTitle: 'english developer', linkedin: 'www.linkedin.com', checked: false },
   ]);
   const [selectAll, setSelectAll] = useState(false);
-
+// const [indeterminate,setIndeterminate]= useState(false);
   const handleCheckboxChange = (index) => {
     const newProfiles = [...profiles];
     newProfiles[index].checked = !newProfiles[index].checked;
-    setProfiles(newProfiles);
+    setProfiles([...newProfiles]);
   };
+
+  // const handleSelectAllChange = (event) => {
+  //   const newProfiles = profiles.map((profile) => ({
+  //     ...profile,
+  //     checked: event.target.checked,
+  //   }));
+  //   setProfiles(newProfiles);
+  //   setSelectAll(event.target.checked);
+  // };
 
   const handleSelectAllChange = (event) => {
     const newProfiles = profiles.map((profile) => ({
@@ -122,7 +36,15 @@ const Popup = () => {
       checked: event.target.checked,
     }));
     setProfiles(newProfiles);
-    setSelectAll(event.target.checked);
+    
+    const allChecked = newProfiles.every((profile) => profile.checked);
+    const someChecked = newProfiles.some((profile) => profile.checked);
+    setSelectAll(allChecked);
+    if (!allChecked && someChecked) {
+      event.target.indeterminate = true;
+    } else {
+      event.target.indeterminate = false;
+    }
   };
 
   const checkedProfiles = profiles.filter((profile) => profile.checked && profile.name !== "Select All");
@@ -132,10 +54,12 @@ const Popup = () => {
     <div key={index}>
       <input
         type="checkbox"
-        checked={profile.checked}
+         checked={profile.checked}
         onChange={() => handleCheckboxChange(index)}
         className="default:ring-2 ..."
         id={profile.name}
+        // indeterminate={!selectAll && count > 0}
+        
       />
       <label htmlFor={profile.name}>
         {profile.name} <br />
@@ -154,19 +78,31 @@ const Popup = () => {
         <img id="img-logo-icon" src={img} alt="binoculars" />
         <img id="img-logo-txt" src={image} alt="Ollopa.com" />
       </div>
+
       <div id="Part2nd">
+
         <div id="box-name">
+
           <div>
-            <input type="checkbox" checked={selectAll} onChange={handleSelectAllChange} id="select-all" />
+
+            <input type="checkbox"  checked={selectAll} onChange={handleSelectAllChange} id="select-all" />
             <label id="all-select" htmlFor="select-all">
               Select All
             </label>
+
           </div>
           {profileList}
         </div>
+
         {shouldShowButton && (
           <div id="button-placement">
-            <button id="button">Export<br/>{count > 0 && `${count} selected`} to<br/>Outreach</button>
+            <button id="button">Export<br/>{count > 0 && `${count} selected to`}<br/>Outreach</button>
+          </div>
+        )}
+
+{shouldShowButton && (
+          <div id="button-placement2">
+            <button id="button2">Download<br/>{count > 0 && `${count} selected`}<br/>information</button>
           </div>
         )}
       </div>
